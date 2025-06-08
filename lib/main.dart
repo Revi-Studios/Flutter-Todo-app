5c3adeb1c7a747d92add0b44c4ac4c4b4fcac520
Revi Studio's Owner <revistudios732@gmail.com>
2025-06-08 18:35:39 +0200

    Added a simple task creating page for creating new tasks with a title, description and a date for when it should be done

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       darkTheme: ThemeData(colorScheme: ColorScheme.dark(primary: Colors.white)),
-      // routes: {
-      //   'createPage' (context) => CreateingPage()
-      // },
+      routes: {
+        '/task_creation': (context) => TaskCreationPage(),
+      },
       home: TodoPage(home: ''),
 
     );
@@ -58,14 +58,60 @@ class TodoPage extends StatelessWidget {
 
 
       floatingActionButton: FloatingActionButton(
-          onPressed:() {
-            // Navigator.pushNamed(context, 'createPage');
-          },
+          onPressed:() => Navigator.pushNamed(context, '/task_creation'),
           child: Icon(Symbols.add),
       ),
 
       
 
+    );
+  }
+}
+
+
+class TaskCreationPage extends StatelessWidget {
+  const TaskCreationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Create Task'), centerTitle: true,),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+
+            children: [
+              
+              Text('Enter a title for the task:'),
+
+              TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Title'), controller: TextEditingController(),),
+
+              TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Description'),),
+
+              OutlinedButton(
+                onPressed: () {showDatePicker(context: context, firstDate: DateTime.now(), lastDate: DateTime(3000, 1, 1)); }, 
+                style: ButtonStyle(),
+                
+                
+                child: Text('Select Date'),
+                ),
+              
+              Padding(
+                padding: const EdgeInsets.only(top: 80),
+                child: FilledButton(onPressed:() {
+                  
+                }, child: Text('Create Task')),
+              )
+
+
+            ],
+          ),
+        )
+
+
+        ),
+      
     );
   }
 }
