bf2f6c0abc73358c749ffe2341ad987515c3e1e3
Revi Studio's Owner <revistudios732@gmail.com>
2025-06-08 21:39:27 +0200

    Created a settings page with a widget class to create list items with a switch, title and subtitle.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
@@ -8,15 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData(colorScheme: ColorScheme.dark(primary: Colors.white)),
+      theme: ThemeData(colorScheme: ColorScheme.light(primary: Colors.black)) ,
+      darkTheme: ThemeData(colorScheme: ColorScheme.dark(primary: Colors.white,)),
       routes: {
         '/task_creation': (context) => TaskCreationPage(),
+        '/settings': (context) => SettingsPage(),
       },
       home: TodoPage(home: ''),
 
@@ -49,13 +52,19 @@ class TodoPage extends StatelessWidget {
         titleTextStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
 
-      drawer: Drawer(),
-
-      body: Column(
+      drawer: Drawer(
+        child: Column(
         children: [
+          DrawerHeader(child: Text('App drawer')),
+
+          ListTile(
+            leading: Icon(Symbols.settings),
+            title: Text('Settings'),
+            onTap:() => Navigator.pushNamed(context, '/settings'),
+          )
         ],
       ),
-
+      ),
 
       floatingActionButton: FloatingActionButton(
           onPressed:() => Navigator.pushNamed(context, '/task_creation'),
@@ -112,6 +121,108 @@ class TaskCreationPage extends StatelessWidget {
 
         ),
       
+    );
+  }
+}
+
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  // Variables
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        centerTitle: true,
+      ),
+
+      body: ListView(
+        children: [
+
+          SwitchSettingsPare(title: 'Dark Mode', subTitle: 'Turns on or off dark mode in the app', isToggled: true,),
+
+          Divider(),
+
+          // SwitchSettingsPare(title: 'Send Notifications', subTitle: 'Sends or stops the sending of notifications', isToggled: true,),
+
+          // Divider(),
+
+
+
+        ],
+      ),
+    );
+  }
+}
+
+class SwitchSettingsPare extends StatefulWidget {
+
+  final String title;
+
+  final String? subTitle;
+
+  final bool? isToggled;
+
+  
+
+  const SwitchSettingsPare({super.key, required this.title, this.subTitle, this.isToggled, });
+
+  @override
+  State<SwitchSettingsPare> createState() => _SwitchSettingsPareState(title: title, subTitle: subTitle, isToggled: isToggled);
+}
+
+class _SwitchSettingsPareState extends State<SwitchSettingsPare> {
+
+  final String title;
+
+  final String? subTitle;
+
+  bool? isToggled;
+
+  _SwitchSettingsPareState({required this.title, this.subTitle, this.isToggled});
+
+  ifNotNull({var value, String type = 'String'}) {
+    switch (type) {
+      case 'String':
+          if (value == null) {
+            return ('');
+          } else {
+            return value;
+          }
+      case 'bool':
+          if (value == null) {
+            return (false);
+          } else {
+            return value;
+          }
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+
+      leading: Switch(
+
+        thumbIcon: WidgetStateProperty.fromMap(<WidgetStatesConstraint, Icon>{WidgetState.selected: Icon(Symbols.check), WidgetState.any: Icon(Symbols.close)}),
+
+      value: ifNotNull(value: isToggled, type: "bool"), 
+
+      onChanged: (bool newValue) {
+        setState(() {
+          isToggled = newValue;
+        });
+      }),
+
+      title: Text(ifNotNull(value: title)),
+
+      subtitle: Text(ifNotNull(value: subTitle)),
+      
+
+
     );
   }
 }
