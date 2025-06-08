7c85c9acd56a7cb843f2ab246e0ea3ac3bf59c65
Revi Studio's Owner <revistudios732@gmail.com>
2025-06-08 16:33:11 +0200

    Made a todo home screen and started on routing and building a task creating screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +8,64 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData(colorScheme: ColorScheme.dark(primary: Colors.white)),
+      // routes: {
+      //   'createPage' (context) => CreateingPage()
+      // },
+      home: TodoPage(home: ''),
+
+    );
+  }
+}
+
+
+class TodoPage extends StatelessWidget {
+
+  final String home;
+
+  const TodoPage({super.key, required this.home,});
+
+
+  String todoPageTitle() {
+    if (home == '') {
+      return ('Todo');
+    } else {
+      return ('Todo ($home)');
+    }
+    }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todoPageTitle()),
+        centerTitle: true,
+        titleTextStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+
+      drawer: Drawer(),
+
+      body: Column(
+        children: [
+        ],
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+          onPressed:() {
+            // Navigator.pushNamed(context, 'createPage');
+          },
+          child: Icon(Symbols.add),
+      ),
+
+      
+
     );
   }
 }
