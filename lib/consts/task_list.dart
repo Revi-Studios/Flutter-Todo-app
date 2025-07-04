d23586f9b5e6857a8e1492140f754325a9c41bbd
Revi Studio's Owner <revistudios732@gmail.com>
2025-07-04 20:09:35 +0200

    Removed the temporary data in the taskList List

--- lib/consts/task_list.dart
+++ lib/consts/task_list.dart
@@ -1,13 +1,2 @@
 // Task List Data
-List<dynamic> taskList = [
-  {
-    'title': 'Test Task',
-    'description': 'This is a test Task ',
-    'checked': false,
-  },
-  {
-    'title': 'nice task',
-    'description': 'this is a nice task :)',
-    'checked': true,
-  },
-];
+List<dynamic> taskList = [];
