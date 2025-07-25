54b7ed1218b013da4c1e39806c7e62bd7f0fed9c
Revi Studio's Owner <revistudios732@gmail.com>
2025-07-25 13:06:10 +0200

    Made a prefrenceData class that will hold the apps data in the future

---
+++ lib/consts/prefrence_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+PrefrenceData prefrenceData = PrefrenceData();
+
+class PrefrenceData {
+
+  final String taskListStorageKey = "taskListData";
+  ThemeMode theme = ThemeMode.system;
+  
+
+
+}
